2ee6d1e049b6767237f23866951c1095ed1c466c
ByungjinKang <oouleos4153@gmail.com>
2023-08-09 10:37:52 +0900

    오류 수정

--- src/component/chat.js
+++ src/component/chat.js
@@ -2,85 +2,14 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { useParams } from 'react-router-dom';
 
-const API_KEY = "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";
-
-const systemMessage = {
-  role: "system",
-  content:
-    "Explain things like you're talking to a software professional with 2 years of experience.",
-};
-
 const Chat = () => {
   const { chatName } = useParams();
 
   const chatMessages = {
-    // 챗봇의 메시지 내용...
   };
   
   const botMessages = {
-    // 봇의 메시지 내용...
   };
-
-
-    const newMessages = [...messages, newMessage];
-
-    setMessages(newMessages);
-
-    // Initial system message to determine ChatGPT functionality
-    // How it responds, how it talks, etc.
-    setIsTyping(true);
-    await processMessageToChatGPT(newMessages);  
-
-  async function processMessageToChatGPT(chatMessages) {
-    // messages is an array of messages
-    // Format messages for chatGPT API
-    // API is expecting objects in format of { role: "user" or "assistant", "content": "message here"}
-    // So we need to reformat
-
-    let apiMessages = chatMessages.map((messageObject) => {
-      let role = "";
-      if (messageObject.sender === "ChatGPT") {
-        role = "assistant";
-      } else {
-        role = "user";
-      }
-      return { role: role, content: messageObject.message };
-    });
-
-    // Get the request body set up with the model we plan to use
-    // and the messages which we formatted above. We add a system message in the front to'
-    // determine how we want chatGPT to act.
-    const apiRequestBody = {
-      model: "gpt-3.5-turbo",
-      messages: [
-        systemMessage, // The system message DEFINES the logic of our chatGPT
-        ...apiMessages, // The messages from our chat with ChatGPT
-      ],
-    };
-
-    await fetch("https://api.openai.com/v1/chat/completions", {
-      method: "POST",
-      headers: {
-        Authorization: "Bearer " + API_KEY,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(apiRequestBody),
-    })
-      .then((data) => {
-        return data.json();
-      })
-      .then((data) => {
-        console.log(data);
-        setMessages([
-          ...chatMessages,
-          {
-            message: data.choices[0].message.content,
-            sender: "ChatGPT",
-          },
-        ]);
-        setIsTyping(false);
-      });
-  }
   
   const initialMessages = chatMessages[chatName]
     ? [...chatMessages[chatName]]
@@ -96,17 +25,10 @@ const Chat = () => {
   const chatWrapperRef = useRef(null);
 
   useEffect(() => {
-    // 메시지가 추가될 때마다 스크롤을 아래로 이동
+
     scrollToBottom();
   }, [messages]);
 
-  //GPT가 보내주는 메시지
-  useEffect(() => {
-
-    return () => {
-      clearInterval(botInterval);
-    };
-  }, []);
 
   const scrollToBottom = () => {
     if (chatWrapperRef.current) {
